7963151940e65eae0f84704ae47ccc97b8d77d17
Craig McPartland <craigmcpd@outlook.com>
2024-07-16 23:31:56 +0100

    Added type hints.

--- Wordsearch.py
+++ Wordsearch.py
@@ -3,11 +3,13 @@ from math import sqrt
 from random import choice, randint
 from sys import argv, exit, stderr
 
-def show_grid(grid, words):
-    grid_size = int(sqrt(len(grid)))
+def show_grid(grid: list[str], words: list[str]) -> None:
+    grid_size: int = int(sqrt(len(grid)))
 
     for row in range(grid_size):
+        row: int
         for column in range(grid_size):
+            column: int
             print(grid[row * grid_size + column], ' ' , end = '')
 
         if row < len(words):
@@ -15,18 +17,21 @@ def show_grid(grid, words):
 
         print()
 
-def fill_grid(grid):
+def fill_grid(grid: list[str]) -> None:
     for r in range(len(grid)):
+        r: int
         grid[r] = grid[r] == ' ' and chr(randint(ord('A'), ord('Z'))) or grid[r]
 
-def try_word(grid, word, x, y, route):
-    grid_size = int(sqrt(len(grid)))
-    grid_copy = []
+def try_word(grid: list[str], word: str, x: int, y: int, route: tuple[int, int]) -> bool:
+    grid_size: int = int(sqrt(len(grid)))
+    grid_copy: list[str] = []
 
     for g in grid:
+        g: str
         grid_copy.append(g)
 
     for c in word:
+        c: str
         if grid_copy[y * grid_size + x] != ' ' and grid_copy[y * grid_size + x] != c:
             return False
 
@@ -35,18 +40,21 @@ def try_word(grid, word, x, y, route):
         y += route[1]
 
     for i in range(len(grid)):
+        i: int
         grid[i] = grid_copy[i]
     
     return True
 
-def add_word_to_grid(grid, word):
-    status = False
-    count = 0   
-    direction = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
-    grid_size = int(sqrt(len(grid)))
+def add_word_to_grid(grid: list[str], word: str) -> bool:
+    rand_x: int
+    rand_y: int
+    status: bool = False
+    count: int = 0   
+    direction: list[tuple[int, int]] = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    grid_size: int = int(sqrt(len(grid)))
 
     while not status and count < 100:
-        route = choice(direction)
+        route: tuple[int, int] = choice(direction)
 
         if route == (0, 1): # n-s
             rand_x = randint(0, grid_size - 1)
@@ -89,7 +97,7 @@ def add_word_to_grid(grid, word):
 
     return True
 
-def read_words_file(filename):
+def read_words_file(filename: str) -> list[str]:
     try:
         with open(filename, 'r') as words_file:
             return words_file.read().splitlines()
@@ -104,13 +112,17 @@ def main():
     
     try:
         opts, args = getopt(argv[1:], "g:")
+        opts: list[tuple[str, str]]
+        args: list[str]
     except GetoptError as err:
         print(argv[0], ':', err)
         exit(3)
 
-    grid_size = 20
+    grid_size: int = 20
 
     for o, a in opts:
+        o: str
+        a: str
         if o == "-g":
             try:
                 grid_size = int(a)
@@ -121,16 +133,17 @@ def main():
             print(argv[0], ': Unknown option. Quitting.', file = stderr)
             exit(5)
 
-    words = read_words_file(args[0])
+    words: list[str] = read_words_file(args[0])
 
     if grid_size < len(words):
         grid_size = len(words)
         print(argv[0], ': Increasing grid size to', grid_size, 'as there are too many words for current size.', file = stderr)
 
-    grid =  [' '] * grid_size * grid_size
-    words_to_remove = []
+    grid: list[str] =  [' '] * grid_size * grid_size
+    words_to_remove: list[str] = []
 
     for word in words:
+        word: str
         if len(word) >= grid_size:
             print(argv[0], ': Dropping word', word, "as it's too long.", file = stderr)
             words_to_remove.append(word)
@@ -139,6 +152,7 @@ def main():
                 words_to_remove.append(word)
 
     for word in words_to_remove:
+        word: str
         words.remove(word)
 
     if (len(words) == 0):
